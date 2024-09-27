614ff4e2a181a684d87b2c742e1effe7006576c9
Luis Gamarra <luisgamarrarosado@gmail.com>
2024-09-27 16:38:45 -0500

    Primer commit - Web API .NET

---
+++ UPC.APIBusiness/UPC.APIBusiness.DBEntity/Model/EntityProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBEntity
+{
+    public class EntityProduct
+    {
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria  { get; set;}
+        public bool EsNuevo { get; set; }
+        public bool EstaEnDescuento { get; set; }
+
+    }
+}
